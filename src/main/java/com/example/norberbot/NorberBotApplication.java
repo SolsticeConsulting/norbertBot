467d8bcd735484db4b455e8522669aeea3cda3d7
AlassTian <sebastianalasia@kinandcarta.com>
2022-02-09 12:58:34 -0300

    initial commit

---
+++ src/main/java/com/example/norberbot/NorberBotApplication.java
@@ -0,0 +1,15 @@
+package com.example.norberbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class NorberBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorberBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/norberbot/NorberBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.norberbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorberBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
