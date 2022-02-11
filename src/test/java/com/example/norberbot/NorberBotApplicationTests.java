051c927c8d66914d31364dbf54642ec3f223182f
AlassTian <sebastianalasia@kinandcarta.com>
2022-02-11 11:38:36 -0300

    changed git.ignore / build.graddle

--- src/test/java/com/example/norberbot/NorberBotApplicationTests.java
+++ src/test/java/com/example/norberbot/NorberBotApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.norberbot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NorberBotApplicationTests {
 
     @Test
