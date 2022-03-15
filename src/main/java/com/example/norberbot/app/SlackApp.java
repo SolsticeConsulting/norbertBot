d92a0fff2527f547e49d00a26c80bd677eada288
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-03-15 08:25:51 -0500

    Fix issue with mention handler

--- src/main/java/com/example/norberbot/app/SlackApp.java
+++ src/main/java/com/example/norberbot/app/SlackApp.java
@@ -3,13 +3,11 @@ package com.example.norberbot.app;
 import com.example.norberbot.handler.BlockActionHandler;
 import com.example.norberbot.handler.MentionHandler;
 import com.slack.api.bolt.App;
-import com.slack.api.methods.SlackApiException;
 import com.slack.api.model.event.AppMentionEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
 import java.util.regex.Pattern;
 
 @Configuration
@@ -28,32 +26,13 @@ public class SlackApp {
             return ctx.ack("What's up?");
         });
 
-        app.message("hello", (req, ctx) -> {
-            var logger = ctx.logger;
-            try {
-                var event = req.getEvent();
-                // Call the chat.postMessage method using the built-in WebClient
-                var result = ctx.client().chatPostMessage(r -> r
-                        // The token you used to initialize your app is stored in the `context` object
-                        .token(ctx.getBotToken())
-                        // Payload message should be posted in the channel where original message was heard
-                        .channel(event.getChannel())
-                        .text("world")
-                );
-                logger.info("result: {}", result);
-            } catch (IOException | SlackApiException e) {
-                logger.error("error: {}", e.getMessage(), e);
-            }
-            return ctx.ack();
-        });
-
         app.event(AppMentionEvent.class, (payload, ctx) -> {
             AppMentionEvent incomingEvent = payload.getEvent();
-            ctx.client().chatPostEphemeral(r -> r
-                    .token(System.getenv("SLACK_BOT_TOKEN"))
-                    .channel(incomingEvent.getChannel())
-                    .text("hOLAAASDFASD")
-            );
+            String[] mention = incomingEvent.getText().split("<+@+\\w+>+");
+            if (mention.length > 1) {
+                String content = mention[1];
+                mentionHandler.handle(incomingEvent, content, ctx.client());
+            }
             return ctx.ack();
         });
 
